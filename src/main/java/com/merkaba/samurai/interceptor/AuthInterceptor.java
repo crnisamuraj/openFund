0d77efded2edde99bcdc8b172988837070b55e20
crnisamuraj <m1ivancevic@gmail.com>
2019-04-29 12:01:48 +0200

    Add potential unfinished AuthInterceptor

---
+++ src/main/java/com/merkaba/samurai/interceptor/AuthInterceptor.java
@@ -0,0 +1,15 @@
+package com.merkaba.samurai.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class AuthInterceptor {
+	/*
+	public Object auth(HttpServletRequest request, Invocation ctx) {
+		HttpSession session = request.getSession();
+		return ctx.proceed();
+	}*/
+	/*PROBSBLY NO NRRF FOR <THIS*/
+}
+
+
