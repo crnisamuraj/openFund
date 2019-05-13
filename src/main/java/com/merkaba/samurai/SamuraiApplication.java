468e105a77370db0bac11e0f6b3bbd9b3fb38e5b
crnisamuraj <m1ivancevic@gmail.com>
2019-05-13 17:13:04 +0200

    add dependencies for date as iso date and enable it

--- src/main/java/com/merkaba/samurai/SamuraiApplication.java
+++ src/main/java/com/merkaba/samurai/SamuraiApplication.java
@@ -1,12 +1,24 @@
 package com.merkaba.samurai;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan(basePackageClasses = {SamuraiApplication.class, Jsr310JpaConverters.class})
 public class SamuraiApplication {
+
+	@PostConstruct
+	void init () {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Belgrade"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamuraiApplication.class, args);
