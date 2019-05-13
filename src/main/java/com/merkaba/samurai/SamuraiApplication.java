537ca01319f00a1489a3952224727d3936ef1932
crnisamuraj <m1ivancevic@gmail.com>
2019-05-13 18:55:27 +0200

    move JpaAuditing to separate config class

--- src/main/java/com/merkaba/samurai/SamuraiApplication.java
+++ src/main/java/com/merkaba/samurai/SamuraiApplication.java
@@ -8,10 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EntityScan(basePackageClasses = {SamuraiApplication.class, Jsr310JpaConverters.class})
 public class SamuraiApplication {
 
